0ca3a170469a4de3bfc35aee5696457f25fa744d
Dani Gábor <rn6m8t@inf.elte.hu>
2022-04-06 14:33:55 +0200

    Added LoginViewModel
    Added LoginViewModel, from now on the Login view will use this instead of the User DatabaseModel directly
    fixes #27

--- NoMoreYoyo/Controllers/PublicController.cs
+++ NoMoreYoyo/Controllers/PublicController.cs
@@ -22,18 +22,32 @@ namespace NoMoreYoyo.Controllers
             return View();
         }
 
-        public IActionResult Login()
+        public IActionResult Login(LoginViewModel model)
         {
-            return View();
+            return View(model);
         }
 
         [HttpPost]
-        public ActionResult SignIn()
+        public ActionResult SignIn(LoginViewModel model)
         {
+            if (model.UserName == null)
+            {
+                ModelState.AddModelError(nameof(model.UserName), "You must provide a username if you wish to log in!");
+            }
+            if (model.Password == null)
+            {
+                ModelState.AddModelError(nameof(model.Password), "You must provide a password if you wish to log in!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(nameof(Login), model);
+            }
+
             return RedirectToAction(nameof(BodyAttributes));
         }
 
-        public IActionResult Signup()
+        public IActionResult Signup(LoginViewModel model)
         {
 
             //TO DO
