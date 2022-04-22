87db1227495a8fc2fd780b9a17e6dced635141e3
JaskierDan <35999247+JaskierDan@users.noreply.github.com>
2022-04-22 14:57:49 +0000

    Deploying to gh-pages from  @ e38e4adea6b4f9c546def8af307c3fa5338b1cf6 🚀

---
+++ NoMoreYoyo/Models/ViewModels/Public/CaloriesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace NoMoreYoyo.Models
+{
+    public class CaloriesViewModel
+    {
+        [DisplayName("Calories (kcal)")]
+        public decimal Calories { get; set; }
+        [DisplayName("Proteins (g)")]
+        public decimal Proteins { get; set; }
+        [DisplayName("Carbohydrates (g)")]
+        public decimal Carbohydrates { get; set; }
+        [DisplayName("Fats (g)")]
+        public decimal Fats { get; set; }
+        [DisplayName("Sex")]
+        public int Sex { get; set; }
+        [DisplayName("Weight")]
+        public decimal Weight { get; set; }
+        [DisplayName("Height")]
+        public decimal Height { get; set; }
+        [DisplayName("Age")]
+        public int Age { get; set; }
+        [DisplayName("Activity")]
+        public decimal? Activity { get; set; }
+    }
+}
