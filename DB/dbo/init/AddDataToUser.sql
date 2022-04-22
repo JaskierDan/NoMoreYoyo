947ed4a9d6b271ca305f79d14e4266f35c3c73de
Dani Gábor <rn6m8t@inf.elte.hu>
2022-04-22 17:06:51 +0200

    gh-pages branch cleared

--- DB/dbo/init/AddDataToUser.sql
+++
@@ -1,5 +0,0 @@
-﻿use [NoMoreYoyo]
-DELETE FROM [NoMoreYoyo].[dbo].[User]
-DBCC CHECKIDENT ('[NoMoreYoyo].[dbo].[User]', RESEED, 0);
-INSERT INTO [NoMoreYoyo].[dbo].[User]([UserName],[EmailAddress],[Password],[Weight],[Height],[Sex],[RegisteredDate],[Age]) VALUES ('Test','test@test.com','DORKKDPLRLDTMLMDPM',75,175,1,'2022-03-22',20);
-INSERT INTO [NoMoreYoyo].[dbo].[User]([UserName],[EmailAddress],[Password],[Weight],[Height],[Sex],[RegisteredDate],[Age]) VALUES ('Admin','admin@test.com','DORKKDPLRLDTMLMDPMOK',75,175,1,'2022-03-22',20);
