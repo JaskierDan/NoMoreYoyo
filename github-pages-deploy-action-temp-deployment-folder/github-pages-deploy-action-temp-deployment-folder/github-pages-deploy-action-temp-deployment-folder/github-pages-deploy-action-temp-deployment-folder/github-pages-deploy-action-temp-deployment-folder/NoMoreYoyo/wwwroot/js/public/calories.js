c7024ecfe7c9db32ffd0cd27ac24369ad82e2301
JaskierDan <35999247+JaskierDan@users.noreply.github.com>
2022-05-10 07:40:48 +0000

    Deploying to gh-pages from  @ bb0ed1c555cf707be2e0bd9d0bc97830db3840b1 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/NoMoreYoyo/wwwroot/js/public/calories.js
@@ -0,0 +1,32 @@
+﻿function CreatePostData() {
+    let data = $('#calculator').serialize();
+
+    return data;
+}
+
+function setupEvents() {
+    let calculateBtn = document.querySelector('#calculateCalories');
+
+    calculateBtn.addEventListener('click', function () {
+        postData();
+    });
+}
+
+function postData() {
+    $.ajax({
+        url: "CalculateCalories",
+        type: "POST",
+        data: CreatePostData(),
+        success: function (response) {
+            if (response.success) {
+                alert("Calories: " + response.calories);
+            }
+            else {
+                $('.card').html(response);
+                setupEvents();
+            }
+        }
+    });
+}
+
+setupEvents();
