ad0b22e90e07702facc4c10cf3d4d1b0659f9cd3
JaskierDan <35999247+JaskierDan@users.noreply.github.com>
2022-05-12 11:56:46 +0000

    Deploying to gh-pages from  @ dfd6ef8c51b310983e69e209cfa653d9938f1481 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/DB/dbo/Tables/User.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]       NVARCHAR (50)  NOT NULL,
+    [EmailAddress]   NVARCHAR (MAX) NOT NULL,
+    [Password]       NVARCHAR (MAX) NOT NULL,
+    [Weight]         DECIMAL (6)    NULL,
+    [Height]         DECIMAL (6)    NULL,
+    [Sex]            BIT            NOT NULL,
+    [RegisteredDate] DATETIME       NOT NULL,
+    [Age]            INT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
