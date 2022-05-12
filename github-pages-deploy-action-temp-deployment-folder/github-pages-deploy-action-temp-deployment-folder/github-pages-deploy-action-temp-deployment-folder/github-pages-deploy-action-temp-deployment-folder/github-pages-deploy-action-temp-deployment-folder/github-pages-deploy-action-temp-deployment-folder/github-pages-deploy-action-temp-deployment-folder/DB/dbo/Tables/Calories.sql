7f8b023c4ccf3162b0ce3898f071f3119192cb69
JaskierDan <35999247+JaskierDan@users.noreply.github.com>
2022-05-12 10:51:11 +0000

    Deploying to gh-pages from  @ c490c98ef2a5a8834099a245ae10bee9aa8cf548 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/DB/dbo/Tables/Calories.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Calories] (
+    [Id]            INT         IDENTITY (1, 1) NOT NULL,
+    [UserId]        INT         NOT NULL,
+    [Date]          DATETIME    NOT NULL,
+    [Amount]        DECIMAL (6) NOT NULL,
+    [Fats]          DECIMAL (6) NULL,
+    [Carbohydrates] DECIMAL (6) NULL,
+    [Proteins]      DECIMAL (6) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
+);
+
