1796f6a792dc46e800180d6177971f36acf3c766
Vivka02 <47754199+Vivka02@users.noreply.github.com>
2022-05-13 11:59:07 +0000

    Deploying to gh-pages from  @ d4f099585d054234e8c2c7152580c4a2963e2a72 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/NoMoreYoyo/Migrations/20220302204613_AddAgeToUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NoMoreYoyo.Migrations
+{
+    public partial class AddAgeToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "User",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "User");
+        }
+    }
+}
---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/NoMoreYoyo/Models/DatabaseModels/BodyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+#nullable disable
+
+namespace NoMoreYoyo.Models
+{
+    public partial class BodyAttribute
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MeasurementTypeId { get; set; }
+        public decimal Value { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual MeasurementType MeasurementType { get; set; }
+        public virtual User User { get; set; }
+    }
+}
